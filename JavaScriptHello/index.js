9b9cd4e86ed8649701cd3d9cd83094e98951e4fe
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-16 17:19:14 +0530

    16-06

---
+++ JavaScriptHello/index.js
@@ -0,0 +1,32 @@
+
+let a = 0;
+
+
+document.getElementById("counter").innerHTML = a;
+// document.getElementById("p1").innerHTML = "Hello " + fname;
+document.getElementById("increase").onclick = function(){
+    
+    a = Number(a)
+    a++;
+
+    document.getElementById("counter").innerHTML = a;
+}
+
+
+document.getElementById("reset").onclick = function(){
+    
+    a = Number(a)
+    a = 0;
+
+    document.getElementById("counter").innerHTML = a;
+}
+
+
+document.getElementById("decrease").onclick = function(){
+    
+    a = Number(a)
+    a--;
+
+    document.getElementById("counter").innerHTML = a;
+}
+
