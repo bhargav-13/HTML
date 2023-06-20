eb2d99c604bdc8429ffe0b2cbeb361b361107ed5
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-20 12:27:14 +0530

    20-06

---
+++ BlackJack/index.js
@@ -0,0 +1,58 @@
+function getRandomCard(){
+    return Math.floor(Math.random()*13)+1
+}
+
+let fcard = getRandomCard()
+let scard = getRandomCard()
+let cards = [fcard, scard] //array
+
+
+
+let msgEL = document.getElementById("message-el")
+let cardEL = document.getElementById("cards-el")
+let sumEL = document.getElementById("sum-el")
+
+
+let message = ""
+let isAlive = true
+let hasBlackJack = false
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame(){
+
+    if(isAlive){
+        let sum = 0
+        for(i = 0; i<cards.length; i+=1){
+            sum += cards[i];
+        }
+
+        cardEL.textContent = "Cards: " + cards
+        sumEL.textContent = "Sum: " + sum
+
+        if(sum < 21){
+            message = "Do you want to draw a new card?"
+        }
+        else if(sum === 21){
+            message  = "You've got Blackjack!"
+            hasBlackJack = true
+        }
+        else{
+            message = "You're out of the game! You Loose!!"
+            isAlive = false
+        }
+    }
+    else{
+        document.getElementById("error").textContent ="You can't play anymore!!"
+    } 
+    msgEL.textContent = message
+}
+
+
+
+function newCard(){
+    cards.push(getRandomCard())
+    renderGame()
+}
