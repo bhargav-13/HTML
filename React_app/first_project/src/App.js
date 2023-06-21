a46fc5d3bac6eb72d8aaafb66bd0b98a7724e26e
bhargav-13 <bhargavthesiya11@gmail.com>
2023-06-21 18:23:26 +0530

    commits

--- React_app/first_project/src/App.js
+++ React_app/first_project/src/App.js
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
 
-import './App.css';
+//API key:  b41222e3          (temporory)
+
+const API_URL = 'http://www.omdbapi.com?apikey=b41222e3';
+
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>  Hello World </h1>
-    </div>
-  );
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+
+    useEffect(() => {
+        searchMovies('inception');
+    }, [])
+
+    return (
+        <h1>hello</h1>
+    );
 }
 
 export default App;
